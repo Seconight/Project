790be944b090253d39296ab3d849b0b8441c3acc
zoetutu <51016571+zoetutu@users.noreply.github.com>
2021-01-25 18:27:41 +0800

    初始化调整、用户信息界面(登录，人脸上传)
    假数据测试 User.vue中切换role测试老师、学生界面

---
+++ lover/src/vantui/index.js
@@ -0,0 +1,26 @@
+//专门引入vantui组件
+//导入Vant-UI
+import Vue from 'vue'
+import {Button,Icon,Tabbar,TabbarItem,Form,Field,Row,Col,RadioGroup,Radio,Toast,CellGroup,Cell,SwipeCell,Card ,Uploader,Image,Popup,Dialog } from 'vant'
+
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(Form)
+Vue.use(Field)
+Vue.use(Row)
+Vue.use(Col)
+
+
+Vue.use(RadioGroup)
+Vue.use(Radio)
+Vue.use(Toast)
+Vue.use(CellGroup)
+Vue.use(Cell)
+Vue.use(SwipeCell)
+Vue.use(Card)
+Vue.use(Uploader)
+Vue.use(Image)
+Vue.use(Popup)
+Vue.use(Dialog)
